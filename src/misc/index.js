fbf803b0decebfc423a96c904e2252ecf8f520c4
Anthony Pauwels <anthony.pauwels.be@gmail.com>
2026-03-10 19:44:20 +0100

    fix: add missing 'is...' helpers on /misc/index.j

--- src/misc/index.js
+++ src/misc/index.js
@@ -3,6 +3,7 @@ import debounce from "./debounce.js";
 import nextTick from "./nextTick.js";
 import throttle from "./throttle.js";
 import {wait, seconds, milliseconds} from "./wait.js";
+import {isClass, isObject, isPlainObject} from "./is.js";
 
 export default {
     clipboard,
@@ -12,4 +13,7 @@ export default {
     wait,
     seconds,
     milliseconds,
+    isObject,
+    isPlainObject,
+    isClass,
 };
